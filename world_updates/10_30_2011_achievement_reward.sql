c6175cf0fe5620320d4296b2ee4c64a3ca11abb2
DJScias <DJScias@hotmail.com>
2011-10-30 21:57:36 +0100

    DB/Achievement: Properly fixed "Heroic: Sinestra" this time, thanks to Subv and apologies for the mistake

--- world_updates/10_30_2011_achievement_reward.sql
+++ world_updates/10_30_2011_achievement_reward.sql
@@ -1,3 +1,7 @@
 -- Heroic: Sinestra title fix
 INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `sender`, `subject`, `text`) VALUES
 (5121, 228, 228, 0, 0, '', '');
+-- Criteria, thanks to Subv
+INSERT INTO achievement_criteria_data (criteria_id,type,value1,value2,comment) VALUES 
+(14412,12,2,0,'Reward only on Heroic 10 man'),
+(15666,12,3,0,'Reward only in Heroic 25 man');
